33959761a2c89073920cf4f7c0babeb31d0369f1
Manroop22 <77290018+Manroop22@users.noreply.github.com>
2022-12-03 20:56:08 -0800

    the Route(direction is obtained). Only problem is that the use Current location as the origin does not work.

--- app/src/main/java/com/example/todolist/GetOrigin.java
+++ app/src/main/java/com/example/todolist/GetOrigin.java
@@ -3,6 +3,7 @@ package com.example.todolist;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,22 +16,33 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.todolist.databinding.ActivityGetOriginBinding;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.model.TypeFilter;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsLeg;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.DirectionsStep;
+import com.google.maps.model.EncodedPolyline;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class GetOrigin extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityGetOriginBinding binding;
     AutocompleteSupportFragment autocompleteFragment;
-    String selectedOriginName;
-    LatLng selectedOriginLatLng;
+    String OriginName;
+    LatLng OriginLatLng;
     String addedLocationName;
     LatLng addedLocationLatLng;
     String currentLocationName;
@@ -78,13 +90,13 @@ public class GetOrigin extends FragmentActivity implements OnMapReadyCallback {
             public void onPlaceSelected (Place place) {
                 // TODO: Get info about the selected place.
                 String TAG="ANSWERAGYA";
-                selectedOriginName=place.getName();
-                selectedOriginLatLng=place.getLatLng();
+                OriginName=place.getName();
+                OriginLatLng=place.getLatLng();
                 // change needed in names
-                Log.i(TAG, "Place: " + selectedOriginName + selectedOriginLatLng);
+                Log.i(TAG, "Place: " + OriginName + OriginLatLng);
 //                addedLocationName=selectedPlaceName;
 //                addedLocationLatLng=selectedPlaceLatLng;
-                getDirections(selectedOriginName,selectedOriginLatLng);
+                getDirections(OriginName,OriginLatLng);
             }
             @Override
             public void onError (Status status) {
@@ -107,16 +119,87 @@ public class GetOrigin extends FragmentActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng location = new LatLng(taskLocationLat, taskLocationLong);
-        mMap.addMarker(new MarkerOptions().position(location).title("Destination: "+taskLocationName));
+        LatLng location =new LatLng(taskLocationLat,taskLocationLong);
+        mMap.addMarker(new MarkerOptions().position(location).title("Origin: "+taskLocationName));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
     }
     public void getDirections(String name, LatLng latLng){
-        mMap.addMarker(new MarkerOptions().position(latLng).title("Origin: "+name));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        LatLng origin = OriginLatLng;
+        mMap.addMarker(new MarkerOptions().position(origin).title("Origin: "+OriginName));
+
+        LatLng dest = new LatLng(taskLocationLat,taskLocationLong);
+        mMap.addMarker(new MarkerOptions().position(dest).title("Destination: "+taskLocationName));
+
+        LatLng zaragoza = new LatLng(41.648823,-0.889085);
+
+        //Define list to get all latlng for the route
+        List<LatLng> path = new ArrayList();
+
+
+        //Execute Directions API request
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey("AIzaSyCXNeBRQ_evQLmpHZtWFMSlHj5tPYNsAlg")
+                .build();
+        String originString=String.format("%s,%s",origin.latitude,origin.longitude);
+        String destString=String.format("%s,%s",dest.latitude,dest.longitude);
+        DirectionsApiRequest req = DirectionsApi.getDirections(context, originString, destString);
+        try {
+            DirectionsResult res = req.await();
+
+            //Loop through legs and steps to get encoded polylines of each step
+            if (res.routes != null && res.routes.length > 0) {
+                DirectionsRoute route = res.routes[0];
+                if (route.legs !=null) {
+                    for(int i=0; i<route.legs.length; i++) {
+                        DirectionsLeg leg = route.legs[i];
+                        if (leg.steps != null) {
+                            for (int j=0; j<leg.steps.length;j++){
+                                DirectionsStep step = leg.steps[j];
+                                if (step.steps != null && step.steps.length >0) {
+                                    for (int k=0; k<step.steps.length;k++){
+                                        DirectionsStep step1 = step.steps[k];
+                                        EncodedPolyline points1 = step1.polyline;
+                                        if (points1 != null) {
+                                            //Decode polyline and add points to list of route coordinates
+                                            List<com.google.maps.model.LatLng> coords1 = points1.decodePath();
+                                            for (com.google.maps.model.LatLng coord1 : coords1) {
+                                                path.add(new LatLng(coord1.lat, coord1.lng));
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    EncodedPolyline points = step.polyline;
+                                    if (points != null) {
+                                        //Decode polyline and add points to list of route coordinates
+                                        List<com.google.maps.model.LatLng> coords = points.decodePath();
+                                        for (com.google.maps.model.LatLng coord : coords) {
+                                            path.add(new LatLng(coord.lat, coord.lng));
+                                        }
+                                    }
+                                }
+                            }
+                        }}
+                }
+            }
+        } catch(Exception ex) {
+            String TAG="ERROR AGI";
+            Log.e(TAG, ex.getLocalizedMessage());
+        }
+
+        //Draw the polyline
+        if (path.size() > 0) {
+            PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.BLUE).width(5);
+            mMap.addPolyline(opts);
+        }
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(zaragoza, 6));
     }
     public void useCurrentLocation(View view){
-
+        OriginName=currentLocationName;
+        OriginLatLng=currentLocationLatLng;
+        getDirections(OriginName,OriginLatLng);
     }
     public void goBack(View view){
         finish();
